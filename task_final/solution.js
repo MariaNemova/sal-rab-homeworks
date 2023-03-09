49220ad27ba48d4800e072965b74efd916716aed
MariaNemova <marianemova@mail.ru>
2023-03-09 13:24:37 +0300

    task_final_solution

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,3 +33,19 @@
 //    }
 // }
 
+function sendRequest(name, phone, {street, house, entrance, floor, flat}, goods, sum) {
+    let data = {
+        client: name + " " + phone,
+        order: {
+            address: "ул. " + street + ", дом " + house + ", " + entrance + " подъезд, " + floor + " этаж," + " кв. " + flat,
+            sum: sum
+        },
+        goods: []
+    }
+    let countOfGoods = goods.length;
+    for (let i = 0; i < countOfGoods; i++) {
+    data.goods.push({title:goods[i].title, count: goods[i].count});
+}
+let jsonData = JSON.stringify({data});
+return jsonData
+}
